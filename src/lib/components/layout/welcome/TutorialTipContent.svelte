3445178f3571ef538d7f63268a390e460399966f
staFF6773 <nosenivel3@gmail.com>
2026-07-15 22:41:37 -0600

    Implementacion de los demas idiomas en el tutorial

--- src/lib/components/layout/welcome/TutorialTipContent.svelte
+++ src/lib/components/layout/welcome/TutorialTipContent.svelte
@@ -2,6 +2,7 @@
 	import { t, locales } from "$lib/i18n";
 	import { launcherStore } from "$lib/state/state.svelte";
 	import { saveSettings } from "$lib/api/launcherService";
+	import Select from "$lib/components/layout/Select.svelte";
 
 	let {
 		stepKey,
@@ -11,8 +12,12 @@
 		isFirstStep: boolean;
 	} = $props();
 
-	async function setLanguage(lang: string) {
-		launcherStore.settings.language = lang;
+	const languageOptions = locales.map((l) => ({
+		value: l.code,
+		label: l.label,
+	}));
+
+	async function onLanguageChange() {
 		await saveSettings();
 	}
 </script>
@@ -26,14 +31,11 @@
 	</p>
 	{#if isFirstStep}
 		<div class="tut-lang">
-			{#each locales.filter((l) => l.code === "en" || l.code === "es") as lang (lang)}
-				<button
-					type="button"
-					class="tut-lang-btn"
-					class:active={launcherStore.settings.language === lang.code}
-					onclick={() => setLanguage(lang.code)}>{lang.label}</button
-				>
-			{/each}
+			<Select
+				bind:value={launcherStore.settings.language}
+				options={languageOptions}
+				onchange={onLanguageChange}
+			/>
 		</div>
 	{/if}
 </div>
