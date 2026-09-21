3716985dda62c8fffa746cc53d2cf193f411ea3e
Not_staff <108166164+staFF6773@users.noreply.github.com>
2026-09-21 14:34:45 -0600

    Update cargoHash in package.nix

--- dist/nix/package.nix
+++ dist/nix/package.nix
@@ -24,7 +24,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   src = lib.cleanSource ./../..;
 
-  cargoHash = "sha256-Sf0bKNv2fpoRpnKhi/JPhbZWJ6gojTmTqOMTvYlsCtI=";
+  cargoHash = "sha256-RXqSVsXae6KUeo6UivIcSF6RpMle9O3Y9GJ+9VU84Xk="";
 
   nodeModules = stdenv.mkDerivation {
     pname = "${finalAttrs.pname}-node_modules";
